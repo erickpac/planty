680b7ac0e0dc1c846e0e04ca63f142603e368e97
Erick Pac <erickj.pac@gmail.com>
2024-10-10 10:52:10 -0600

    chore: init files

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,41 @@
+import { useUserStore } from "@/store/userStore";
+import { Redirect, Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import Feather from "@expo/vector-icons/Feather";
+import { theme } from "@/theme";
+
+export default function Layout() {
+  const hasFinishedOnboarding = useUserStore(
+    // eslint-disable-next-line prettier/prettier
+    (state) => state.hasFinishedOnboarding
+  );
+
+  if (!hasFinishedOnboarding) {
+    return <Redirect href="/onboarding" />;
+  }
+
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Entypo name="leaf" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
